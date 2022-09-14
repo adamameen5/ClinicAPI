89d515c5e4db4b0c55bd0ab1fbb6f17c0cd43584
Adam Ameen <adamameen5@gmail.com>
2022-09-14 12:12:18 +0530

    Add project files.

---
+++ ClinicAPI/Models/Patient.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClinicAPI.Models
+{
+    public class Patient
+    {
+        public int patientId { get; set; }
+        public string patientName { get; set; }
+        public string patientAddress { get; set; }
+        public string patientEmail { get; set; }
+    }
+}
